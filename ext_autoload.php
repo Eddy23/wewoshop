7092c4a43d28f929b0275b06e5dc1b6e067a060d
Eddy Wolbert <info@eddy-wolbert.de>
2014-06-26 16:32:15 +0200

    ext_autoload neu committed

---
+++ ext_autoload.php
@@ -0,0 +1,5 @@
+<?php
+$libraryClassesPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('wewoshop') . 'Resources/Private/PHP/';
+return array(
+        'FPDF' => $libraryClassesPath . 'Fpdf/fpdf.php',
+);
